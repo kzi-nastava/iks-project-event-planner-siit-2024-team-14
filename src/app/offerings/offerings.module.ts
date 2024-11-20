6c2dfc2d5f48d2c710e33fd54c19341665adf755
Ћетковић Алекса <cetkovic.sv77.2022@uns.ac.rs>
2024-11-20 22:36:43 +0100

    Introduce add service component

--- src/app/offerings/offerings.module.ts
+++ src/app/offerings/offerings.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddServiceComponent } from './add-service/add-service.component';
 import {ReactiveFormsModule} from '@angular/forms';
-import { ServiceDetailsComponent } from './service-details/service-details.component';
 
 
 
 @NgModule({
   declarations: [
+    AddServiceComponent
   ],
   imports: [
     CommonModule,
