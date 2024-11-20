b3b92aa07438da6002867d70f9ec5ec88782f1f4
Tea <belicc.tt@gmail.com>
2024-11-20 16:09:01 +0100

    Commiting Registration Screens

---
+++ src/app/components/registration-eo/registration-eo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-eo',
+  templateUrl: './registration-eo.component.html',
+  styleUrl: './registration-eo.component.css'
+})
+export class RegistrationEoComponent {
+
+}
