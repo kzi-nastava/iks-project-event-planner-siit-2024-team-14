bc97b78d8232dc366957c4f0bcc0dd839fd63213
Tea <belicc.tt@gmail.com>
2025-02-04 23:37:43 +0100

    Commiting routing from login to home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component'
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirect to login on empty path
   { path: 'login', component: LoginComponent },         // Login route
+  { path: 'home', component: HomeComponent }
   // Add other routes if needed
 ];
 
