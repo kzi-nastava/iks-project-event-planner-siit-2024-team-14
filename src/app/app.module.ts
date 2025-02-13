14e4e4207684f61f57ceae69310c88e3c85e8906
milicabosancic1 <milicabosancic1612@gmail.com>
2025-02-13 15:05:24 +0100

    fixed problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';  // Correct import
 import { HomeComponent } from './components/home/home.component';
@@ -10,7 +9,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { MatIcon } from '@angular/material/icon';
 import { NgOptimizedImage } from '@angular/common';
 import { DrawerNavbarComponent } from './components/drawer-navbar/drawer-navbar.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HottestEventsComponent } from './components/homepage/hottest-events/hottest-events.component';
 import { OurEventsComponent } from './components/homepage/our-events/our-events.component';
 import { HottestServicesComponent } from './components/homepage/hottest-services/hottest-services.component';
