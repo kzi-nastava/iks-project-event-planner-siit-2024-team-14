2989721be0145b25062b1995a8eb2ccb9cc0216d
milicabosancic1 <milicabosancic1612@gmail.com>
2025-02-20 21:26:57 +0100

    added notification icon with number of unread notifications

--- src/app/components/home/home-organizer/home-organizer.component.ts
+++ src/app/components/home/home-organizer/home-organizer.component.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NotificationService} from '../../notifications/notifications.service';
 
 @Component({
   selector: 'app-home-guest-organizer',
   templateUrl: './home-organizer.component.html',
   styleUrls: ['./home-organizer.component.css']
 })
-export class HomeOrganizerComponent {
-  title = 'Welcome to the Home Page!';
+export class HomeOrganizerComponent implements OnInit {
   isSidebarOpen: boolean = false;
   isNotificationsOpen: boolean = false;
+
+  unreadCount: number = 0;
+
+  constructor(private notificationService: NotificationService) {}
+
+  ngOnInit(): void {
+    const userId = this.getUserIdFromLocalStorage();
+
+    if (userId !== null) {
+      this.loadUnreadNotificationCount(userId);
+      this.notificationService.unreadNotificationCount$.subscribe(count => {
+        this.unreadCount = count;
+      });
+    }
+  }
+
+  getUserIdFromLocalStorage(): number | null {
+    const userIdFromStorage = localStorage.getItem("userId");
+    if (userIdFromStorage) {
+      const parsedUserId = parseInt(userIdFromStorage, 10);
+      return isNaN(parsedUserId) ? null : parsedUserId;
+    }
+    return null;
+  }
+
+  loadUnreadNotificationCount(userId: number): void {
+    this.notificationService.loadUnreadNotificationsCount(userId);
+  }
 
   toggleSidebar(): void {
     this.isSidebarOpen = !this.isSidebarOpen;
