8a9a8cbf2918fa305d08a80a18d5d0be94f46039
milicabosancic1 <milicabosancic1612@gmail.com>
2025-03-06 18:54:07 +0100

    need to remove visibility of blocked user

--- src/app/components/homepage/hottest-events/hottest-events.service.ts
+++ src/app/components/homepage/hottest-events/hottest-events.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {EventModel} from '../../../interfaces/event.model';
 
 @Injectable({
@@ -9,17 +9,27 @@ import {EventModel} from '../../../interfaces/event.model';
 export class HottestEventsService {
   private apiUrl = 'http://localhost:8080/api/events/top5';
 
+  private blockedUsersUrl = 'http://localhost:8080/api/chat/blocked-users'; // URL za blokirane korisnike
+
   constructor(private http: HttpClient) {}
 
   getTopEvents(): Observable<EventModel[]> {
-    // Preuzimanje grada iz localStorage
     const userCity = localStorage.getItem('userCity');
-
-    // Ako postoji grad korisnika, koristi ga u URL-u
-    const city = userCity ? encodeURIComponent(userCity) : 'Novi Sad';  // Ako nema grada, koristi Novi Sad
+    const city = userCity ? encodeURIComponent(userCity) : 'Novi Sad';
 
     const url = `${this.apiUrl}?city=${city}`;
 
     return this.http.get<EventModel[]>(url);
   }
+
+  getBlockedUsers(): Observable<number[]> {
+    const userId = localStorage.getItem('userId');
+
+    if (!userId) {
+      return of([]); // Vraća Observable sa praznim nizom ako userId nije postavljen
+    }
+    return this.http.get<number[]>(`${this.blockedUsersUrl}?userId=${userId}`);
+  }
+
+
 }
