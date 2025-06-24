093ed81958936774742e875673af696446672f27
Алекса Ћетковић <cetkovic.sv77.2022@uns.ac.rs>
2025-06-24 20:20:15 +0200

    Move chat sidebar to communications module

--- src/app/communication/communication.module.ts
+++ src/app/communication/communication.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import {CommonModule} from '@angular/common';
+import {ChatSidebarComponent} from './chat-sidebar/chat-sidebar.component';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ChatSidebarComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+  ],
+  exports: [
+    ChatSidebarComponent,
   ]
 })
 export class CommunicationModule { }
