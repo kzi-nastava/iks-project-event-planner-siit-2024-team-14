86ebd4630dede2365380b2a608b1b0f5a4363061
Алекса Ћетковић <cetkovic.sv77.2022@uns.ac.rs>
2025-07-05 20:30:57 +0200

    Add provider price list component

--- src/app/offerings/service.service.ts
+++ src/app/offerings/service.service.ts
@@ -1,34 +1,38 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Service} from './model/service.model';
 import {Page} from '../interfaces/page.model';
+import {environment} from '../../environment/environment';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
-  private baseURL = 'http://localhost:8080/api/services/';
+  private baseURL = `${environment.apiUrl}/services`;
 
   constructor(private httpClient: HttpClient) { }
 
 
-  getAll(): Observable<Page<Service>[]> {
-    return this.httpClient.get<Page<Service>[]>(this.baseURL);
+  getAll(params?: any): Observable<Page<Service>> {
+    return this.httpClient.get<Page<Service>>(this.baseURL, {params: new HttpParams({fromObject: params})});
   }
 
   getById(id: number): Observable<Service> {
-    return this.httpClient.get<Service>(this.baseURL + id);
+    return this.httpClient.get<Service>(this.baseURL + '/' + id);
   }
 
   getAvailableStartTimes(serviceId: number, date: string, duration: number | null) {
-    let url = `http://localhost:8080/api/bookings/available-start-times?serviceId=${serviceId}&date=${date}`;
+    let url = `${environment.apiUrl}/bookings/available-start-times?serviceId=${serviceId}&date=${date}`;
     if (duration !== null) {
       url += `&duration=${duration}`;
     }
     return this.httpClient.get<string[]>(url);
   }
 
+  update(service: Partial<Service> & Pick<Service, 'id'>) {
+    return this.httpClient.put<Service>(this.baseURL + service.id, service);
+  }
 
 }
